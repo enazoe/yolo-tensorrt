24c58dfa3172da08b59f9cc0d56269bc41536b76
Xu Hao <xuhao3e8@gmail.com>
2020-03-24 06:26:13 +0800

    Works on my PC

--- samples/sample_detector.cpp
+++ samples/sample_detector.cpp
@@ -1,22 +1,56 @@
 #include <time.h>
 #include "class_detector.h"
+#include <ctime>
+#include <cstdlib>
+#include <chrono>
+
+class TicToc
+{
+  public:
+    TicToc()
+    {
+        tic();
+    }
+
+    void tic()
+    {
+        start = std::chrono::system_clock::now();
+    }
+
+    double toc()
+    {
+        end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        return elapsed_seconds.count() * 1000;
+    }
+
+  private:
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+};
 
 int main()
 {
 	Detector detector;
 	Config config;
-	config.file_model_cfg = "../configs/yolov3.cfg";
-	config.file_model_weights = "../configs/yolov3.weights";
+	config.file_model_cfg = "/home/xuhao/swarm_ws/src/swarm_detector/config/drone_yolov3-tiny.cfg";
+	config.file_model_weights = "/home/xuhao/swarm_ws/src/swarm_detector/config/drone_yolov3-tiny.weights";
 	config.calibration_image_list_file_txt = "../configs/calibration_images.txt";
 	config.inference_precison = FP32;
 	detector.init(config);
 
-	cv::Mat mat_image = cv::imread("../configs/dog.jpg", cv::IMREAD_UNCHANGED);
+	cv::Mat mat_image = cv::imread("../configs/drone.jpg", cv::IMREAD_UNCHANGED);
 
 	std::vector<Result> res;
 	std::clock_t t_strat = std::clock();
 	detector.detect(mat_image, res);
-	std::cout << "detect time = " << double(std::clock() - t_strat) << "ms" << std::endl;
+	std::cout << "detect time = " << double(std::clock() - t_strat) << "us" << std::endl;
+	// t_strat = std::clock();
+	for (int i = 0;i < 10; i ++ ) {
+		TicToc tic;
+		detector.detect(mat_image, res);
+		std::cout << "detect time = " << double(tic.toc()) << "ms" << std::endl;
+	}
+	
 	for (const auto &r : res)
 	{
 		std::cout << "id:" << r.id << " prob:" << r.prob << " rect:" << r.rect << std::endl;
